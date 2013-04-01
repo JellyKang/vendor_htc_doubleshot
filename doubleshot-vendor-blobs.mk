5c3aeeac06cb1f12b0fe2ea12792057ea0120828
Emmanuel Utomi <emmanuelutomi@gmail.com>
2013-04-01 17:04:56 -0500

    No need for prebuilt liblights, we use custom sauce now

--- doubleshot-vendor-blobs.mk
+++ doubleshot-vendor-blobs.mk
@@ -106,6 +106,5 @@ PRODUCT_COPY_FILES += \
   vendor/htc/doubleshot/proprietary/lib/egl/libEGL_adreno200.so:system/lib/egl/libEGL_adreno200.so \
   vendor/htc/doubleshot/proprietary/lib/libgsl.so:system/lib/libgsl.so \
   vendor/htc/doubleshot/proprietary/lib/libc2d2_z180.so:system/lib/libc2d2_z180.so \
-  vendor/htc/doubleshot/proprietary/lib/hw/lights.doubleshot.so:system/lib/hw/lights.doubleshot.so \
   vendor/htc/doubleshot/proprietary/lib/hw/sensors.doubleshot.so:system/lib/hw/sensors.doubleshot.so \
   vendor/htc/doubleshot/proprietary/lib/hw/gps.doubleshot.so:system/lib/hw/gps.doubleshot.so
